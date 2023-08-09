c530998338513a5aa02d6f884d57180309a233c0
leandroheffes24 <leandroheffes@outlook.es>
2023-08-09 15:31:09 -0300

    Se creó el servidor en app.js y se creó el archivo styles.css en la carpeta public/css

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require(express)
+const path = require("path")
+const app = express()
+
+// RUTA PÚBLICA ESTÁTICA
+app.use(express.static(path.join(__dirname, "../public")))
+
+//INICIAR SERVIDOR
+const PORT = 3000
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
+
+// RUTAS
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views/home.html")))
