93922a7fa7af2faa17ed5911375b225e24f20735
leandroheffes24 <leandroheffes@outlook.es>
2023-08-15 01:03:42 -0300

    Se creó la página para crear cuenta de usuario y su respectivo formulario

--- src/app.js
+++ src/app.js
@@ -11,3 +11,4 @@ app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
 
 // RUTAS
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views/home.html")))
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views/register.html")))
